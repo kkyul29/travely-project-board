f1dfb803d93228be7c566fc12687f102cb426b3a
Cassie <cassie.kim29@gmail.com>
2023-02-16 16:08:36 +0900

    #1 - Data REST 설정
    게시글, 댓글의 json api 를 자동으로 restful 하게 만들 수 있도록 설정.

--- travely-project-board/src/main/java/travely/travelyprojectboard/repository/ArticleRepository.java
+++ travely-project-board/src/main/java/travely/travelyprojectboard/repository/ArticleRepository.java
@@ -1,7 +1,9 @@
 package travely.travelyprojectboard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import travely.travelyprojectboard.domain.Article;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
