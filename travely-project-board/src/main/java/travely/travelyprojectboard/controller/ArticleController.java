434ecd8e7af7441ef89a093d1c7409173cf85c3b
Cassie <cassie.kim29@gmail.com>
2023-02-21 22:39:20 +0900

    #24 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 'Page' 클래스를 이용하기 때문에, 페이징과 정렬을 함께 테스트 함. 이후 정렬 구현에서 이어서 활용 예정.

--- travely-project-board/src/main/java/travely/travelyprojectboard/controller/ArticleController.java
+++ travely-project-board/src/main/java/travely/travelyprojectboard/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package travely.travelyprojectboard.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +15,9 @@ import travely.travelyprojectboard.domain.type.SearchType;
 import travely.travelyprojectboard.dto.response.ArticleResponse;
 import travely.travelyprojectboard.dto.response.ArticleWithCommentsResponse;
 import travely.travelyprojectboard.service.ArticleService;
+import travely.travelyprojectboard.service.PaginationService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/articles")
@@ -21,6 +25,8 @@ import travely.travelyprojectboard.service.ArticleService;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
+
     @GetMapping
     public String articles(
             @RequestParam(required = false) SearchType searchType,
@@ -28,7 +34,11 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ){
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+            map.addAttribute("articles", articles);
+            map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
--- travely-project-board/src/test/java/travely/travelyprojectboard/controller/ArticleControllerTest.java
+++ travely-project-board/src/test/java/travely/travelyprojectboard/controller/ArticleControllerTest.java
@@ -8,15 +8,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import travely.travelyprojectboard.config.SecurityConfig;
 import travely.travelyprojectboard.dto.ArticleWithCommentsDto;
 import travely.travelyprojectboard.dto.UserAccountDto;
 import travely.travelyprojectboard.service.ArticleService;
+import travely.travelyprojectboard.service.PaginationService;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -33,6 +37,8 @@ class ArticleControllerTest {
     private final MockMvc mvc;
 
     @MockBean private ArticleService articleService;
+    @MockBean private PaginationService paginationService;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -42,14 +48,46 @@ class ArticleControllerTest {
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
         given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0,1,2,3,4));
 
         // When & Then
         mvc.perform(get("/articles"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("paginationBarNumbers"));
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(),anyInt());
+    }
+
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
+    @Test
+    public void givenPagingAndSortingParams_whenSearchingArticlesPage_thenReturnsArticlesPage() throws Exception {
+        // Given
+        String sortName = "title";
+        String direction = "desc";
+        int pageNumber = 0;
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc(sortName)));
+        List<Integer> barNumbers = List.of(1,2,3,4,5);
+        given(articleService.searchArticles(null, null, pageable)).willReturn(Page.empty());
+        given(paginationService.getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages())).willReturn(barNumbers);
+
+        // When & Then
+        mvc.perform(
+                get("/articles")
+                        .queryParam("page", String.valueOf(pageNumber))
+                        .queryParam("size", String.valueOf(pageSize))
+                        .queryParam("sort", sortName + "," + direction)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attribute("paginationBarNumbers", barNumbers));
+        then(articleService).should().searchArticles(null, null, pageable);
+        then(paginationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
