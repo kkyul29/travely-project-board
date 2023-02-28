ee90dbb13a9717acc89de2b617b56b964abdc5c1
Cassie <cassie.kim29@gmail.com>
2023-02-28 19:55:11 +0900

    #45 - 시큐리티 설정과 자바 대응 코드 구현
    스프링 부트 가이드에 맞춰서 시큐리티 설정을 적용하고, 인증 정보를 db로 부터 부르는 빈과 패스워드 인코더 빈까지 등록. 영향 받는 부분을 서비스 코드와 컨트롤러에 반영.

--- travely-project-board/src/main/java/travely/travelyprojectboard/config/JpaConfig.java
+++ travely-project-board/src/main/java/travely/travelyprojectboard/config/JpaConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import travely.travelyprojectboard.dto.security.BoardPrincipal;
 
 import java.util.Optional;
 
@@ -13,7 +17,12 @@ public class JpaConfig {
 
     @Bean
     public AuditorAware<String> auditorAware(){
-        return () -> Optional.of("cassie"); // TODO: 임의데이터. 스프링 시큐리티로 인증 기능을 붙이게 되면, 그 때 수정 필요.
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getPrincipal)
+                .map(BoardPrincipal.class::cast)
+                .map(BoardPrincipal::getUsername);
     }
 
 }
--- travely-project-board/src/main/java/travely/travelyprojectboard/controller/ArticleCommentController.java
+++ travely-project-board/src/main/java/travely/travelyprojectboard/controller/ArticleCommentController.java
@@ -1,12 +1,14 @@
 package travely.travelyprojectboard.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import travely.travelyprojectboard.dto.UserAccountDto;
 import travely.travelyprojectboard.dto.request.ArticleCommentRequest;
+import travely.travelyprojectboard.dto.security.BoardPrincipal;
 import travely.travelyprojectboard.service.ArticleCommentService;
 
 @RequiredArgsConstructor
@@ -17,18 +19,22 @@ public class ArticleCommentController {
     private final ArticleCommentService articleCommentService;
 
     @PostMapping("/new")
-    public String postNewArticleComment(ArticleCommentRequest articleCommentRequest){
-        // TODO: 인증 정보를 넣어줘야 함.
-        articleCommentService.saveArticleComment(articleCommentRequest.toDto(UserAccountDto.of(
-                "cassie", "pw", "cassie@mail.com", null, null
-        )));
+    public String postNewArticleComment(
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+            ArticleCommentRequest articleCommentRequest
+    ){
+        articleCommentService.saveArticleComment(articleCommentRequest.toDto(boardPrincipal.toDto()));
 
         return "redirect:/articles/" + articleCommentRequest.articleId();
     }
 
     @PostMapping("/{commentId}/delete")
-    public String deleteArticleComment(@PathVariable Long commentId, Long articleId) {
-        articleCommentService.deleteArticleComment(commentId);
+    public String deleteArticleComment(
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal BoardPrincipal boardPrincipal,
+            Long articleId
+    ) {
+        articleCommentService.deleteArticleComment(commentId, boardPrincipal.getUsername());
 
         return "redirect:/articles/" + articleId;
     }
